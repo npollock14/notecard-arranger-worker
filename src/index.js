27fe6222dfd9da87caed5b8e4e4e26a8dff7e4bd
Nathan <ndpollock@wpi.edu>
2022-03-28 15:58:15 -0400

    funtionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// const jsdom = require('jsdom')
 const cheerio = require('cheerio')
-// const { JSDOM } = jsdom
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -15,11 +13,31 @@ async function handleRequest(request) {
   )
   const html = await raw_res.text()
   const $ = cheerio.load(html)
-  const text = $('.vs-card')
-    .append('\n\n')
+  let text = $('p')
+    .append('\n')
     .text()
 
-  return new Response(text, {
+  //split text after a "Answer: " with a letter
+  let split_text = text.split(/Answer: /)
+  for (let i = 1; i < split_text.length; i++) {
+    //add the first line of the next text to the previous text
+    split_text[i - 1] += '\nAnswer: ' + split_text[i].split('\n')[0].trim()
+    //remove the first line of the next text
+    split_text[i] = split_text[i]
+      .replace(split_text[i].split('\n')[0], '')
+      .trim()
+  }
+  //remove the last element of the array
+  split_text.pop()
+
+  //sort the array of split text with a function
+  split_text.sort(function(a, b) {
+    return a.split(' ')[1].localeCompare(b.split(' ')[1])
+  })
+
+  let finalText = split_text.join('\n')
+
+  return new Response(finalText, {
     headers: { 'content-type': 'text/plain' },
   })
 }
